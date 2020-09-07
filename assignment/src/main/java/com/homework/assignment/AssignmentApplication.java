e4a9c6798ca37d02dbc49a7304911bfd6d4c429f
Unknown <jbk5988@gmail.com>
2020-09-07 17:30:24 -0400

    Implement post endpoint
    Implemented post endpoint, cleaned up unused imports

--- assignment/src/main/java/com/homework/assignment/AssignmentApplication.java
+++ assignment/src/main/java/com/homework/assignment/AssignmentApplication.java
@@ -1,14 +1,7 @@
 package com.homework.assignment;
 
-import com.homework.assignment.entities.Pokemon;
-import com.homework.assignment.mutators.PokemonMutator;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
-import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 
 @SpringBootApplication
--- assignment/src/main/java/com/homework/assignment/controllers/PokedexController.java
+++ assignment/src/main/java/com/homework/assignment/controllers/PokedexController.java
@@ -2,10 +2,15 @@ package com.homework.assignment.controllers;
 
 import com.homework.assignment.entities.Pokemon;
 import com.homework.assignment.persistence.PokemonDAO;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,7 +22,20 @@ public class PokedexController {
     @Autowired
     PokemonDAO pokemonDAO;
 
-    @GetMapping(value="/{id}")
+    @PostMapping("/addpokemon")
+    public String addUser(@RequestBody Pokemon pokemon, BindingResult result)
+            throws CsvRequiredFieldEmptyException, IOException, CsvDataTypeMismatchException {
+        if (result.hasErrors()) {
+            System.out.println(result.getAllErrors());
+            return "/";
+        }
+
+        pokemonDAO.save(pokemon);
+        return "Pokemon created";
+    }
+
+
+    @GetMapping(value="/pokemon/{id}")
     Pokemon getPokemon(@PathVariable Long id) throws IOException {
         return pokemonDAO.get(id).get();
     }
